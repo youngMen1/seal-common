82ebe80d542f52494f9f8a268460306288253d5e
youngMen1 <fengzhiqiangzxx199644>
2019-08-23 11:22:04 +0800

    desc:jackson2.9.9

--- seal-jackson/src/main/java/com/seal/jackson/util/JsonUtils.java
+++ seal-jackson/src/main/java/com/seal/jackson/util/JsonUtils.java
@@ -1,6 +1,9 @@
 package com.seal.jackson.util;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.slf4j.Logger;
@@ -28,6 +31,30 @@ public class JsonUtils {
     public void init() {
         // 转换为格式化的json
         this.objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+//        // 美化输出
+//        this.objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+//        // 允许序列化空的POJO类
+//        // （否则会抛出异常）
+//        this.objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+//        // 把java.util.Date, Calendar输出为数字（时间戳）
+//        this.objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//
+//        // 在遇到未知属性的时候不抛出异常
+//        this.objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+//        // 强制JSON 空字符串("")转换为null对象值:
+//        this.objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+//
+//        // 在JSON中允许C/C++ 样式的注释(非标准，默认禁用)
+//        this.objectMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+//        // 允许没有引号的字段名（非标准）
+//        this.objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+//        // 允许单引号（非标准）
+//        this.objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+//        // 强制转义非ASCII字符
+//        this.objectMapper.configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+//        // 将内容包裹为一个JSON属性，属性名由@JsonRootName注解指定
+//        this.objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, true);
     }
 
 
