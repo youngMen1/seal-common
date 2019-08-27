b7269b7ebc573d5b3d02f19c6c1eb4f308630f27
youngMen1 <fengzhiqiangzxx199644>
2019-08-27 17:11:27 +0800

    desc:常用工具类

--- seal-util/src/main/java/com/seal/util/SealUtilApplication.java
+++ seal-util/src/main/java/com/seal/util/SealUtilApplication.java
@@ -2,7 +2,12 @@ package com.seal.util;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author zhiqiang.feng
+ * @version 1.0
+ * @date-time 2019/7/31 16:40
+ * @description util工具类总结
+ **/
 @SpringBootApplication
 public class SealUtilApplication {
 
