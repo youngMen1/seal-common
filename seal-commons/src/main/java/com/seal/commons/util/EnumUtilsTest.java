a35ec0fd932baa6b41e42a2228945ee9aeea7a40
youngMen1 <fengzhiqiangzxx199644>
2019-08-29 15:30:23 +0800

    desc:util

--- seal-commons/src/main/java/com/seal/commons/util/EnumUtilsTest.java
+++ seal-commons/src/main/java/com/seal/commons/util/EnumUtilsTest.java
@@ -29,14 +29,17 @@ public class EnumUtilsTest {
         ImagesTypeEnum imagesTypeEnum = EnumUtils.getEnum(ImagesTypeEnum.class, "JPG");
         System.out.println("imagesTypeEnum = " + imagesTypeEnum);
         System.out.println("--------------");
+
         List<ImagesTypeEnum> imagesTypeEnumList = EnumUtils.getEnumList(ImagesTypeEnum.class);
         imagesTypeEnumList.stream().forEach(
                 imagesTypeEnum1 -> System.out.println("imagesTypeEnum1 = " + imagesTypeEnum1)
         );
         System.out.println("--------------");
+
         Map<String, ImagesTypeEnum> imagesTypeEnumMap = EnumUtils.getEnumMap(ImagesTypeEnum.class);
         imagesTypeEnumMap.forEach((k, v) -> System.out.println("key：" + k + ",value：" + v));
         System.out.println("-------------");
+
         boolean result = EnumUtils.isValidEnum(ImagesTypeEnum.class, "JPG");
         System.out.println("result = " + result);
         boolean result1 = EnumUtils.isValidEnum(ImagesTypeEnum.class, null);
