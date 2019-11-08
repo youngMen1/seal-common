a5ed6f2229652a3a8b0d6cdc910c776e55730455
youngMen1 <fengzhiqiangzxx199644>
2019-11-08 22:39:16 +0800

    desc:excel

--- seal-hutool/src/main/java/com/seal/hutool/util/ExcelUtilTest.java
+++ seal-hutool/src/main/java/com/seal/hutool/util/ExcelUtilTest.java
@@ -5,7 +5,10 @@ import cn.hutool.core.lang.Console;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.sax.handler.RowHandler;
+import org.junit.Test;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,16 +21,103 @@ public class ExcelUtilTest {
 
     public static void main(String[] args) {
 
-        excelUtilTest();
+        //excelUtilTest();
+        // excelUtilPhoneTest();
+    }
+
+
+    @Test
+    public void excelUtilPhoneTest() {
+        // //默认读全部行列，还可以通过重载函数，第二个参数指定读取的Sheet  手机号.xlsx  交易成功的会员信息.xls  分销关系表数据.xls
+        ExcelReader reader = ExcelUtil.getReader("G:\\交易成功的会员信息.xls");
+        // 行索引  2行开始
+        List<List<Object>> read = reader.read(1);
+        ExcelReader reader2 = ExcelUtil.getReader("G:\\分销关系表数据.xls");
+
+        List<String> stringList = new ArrayList<>();
+
+        // 行索引  2行开始
+        List<List<Object>> read2 = reader2.read(1);
+        List<String> list = new ArrayList<>();
+        for (List<Object> objects : read) {
+            String memberId = objects.get(1).toString();
+            stringList.add(objects.get(0)+"");
+            // 第二列
+            for (List<Object> objects2 : read2) {
+                // 第一列
+                String newUserId = objects2.get(0).toString();
+                if (memberId.equals(newUserId)) {
+                    // 第一列手机号
+                    list.add(objects.get(0) + "");
+                }
+            }
+        }
+
+//
+//        List<String> list1 = new ArrayList();
+//        ExcelReader reader3 = ExcelUtil.getReader("G:\\已发短信的电话号码.xlsx");
+//        // 行索引  2行开始
+//        List<List<Object>> read3 = reader3.read(1);
+//        for (List<Object> objects3 : read3) {
+//            list1.add(objects3.get(0) + "");
+//        }
+        stringList.removeAll(list);
+
+
+//        ExcelWriter writer = ExcelUtil.getWriter("toXls.xls");
+//        //设置sheet名
+//        writer.renameSheet("短信触达的购买用户");
+//        writer.write(list);
+//        writer.close();
+
+        try {
+            writeFileContext(stringList, "新的短信触发的用户.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 将list按行写入到txt文件中
+     *
+     * @param strings
+     * @param path
+     * @throws Exception
+     */
+    public static void writeFileContext(List<String> strings, String path) throws Exception {
+        File file = new File(path);
+        //如果没有文件就创建
+        if (!file.isFile()) {
+            file.createNewFile();
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        for (String basePostParam : strings) {
+            writer.write(basePostParam + "\r\n");
+        }
+        writer.close();
     }
 
 
     private static void excelUtilTest() {
         // 从文件中读取Excel为ExcelReader
-        ExcelReader reader = ExcelUtil.getReader(FileUtil.file("test.xlsx"));
+        ExcelReader reader = ExcelUtil.getReader(FileUtil.file("C:\\手机号.xlsx"));
+
+        List<List<Object>> read = reader.read(1);
+        for (List<Object> objects : read) {
+
+        }
+
+
+        try {
+            InputStream inputStream = new FileInputStream(new File("C:\\手机号.xlsx"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
         // 通过sheet名获取
-        reader = ExcelUtil.getReader(FileUtil.file("test.xlsx"), "sheet1");
+        //  reader = ExcelUtil.getReader(FileUtil.file("test.xlsx"), "sheet1");
 
         // 读取大数据量的Excel
         ExcelUtil.readBySax("aaa.xlsx", 0, createRowHandler());
@@ -36,9 +126,10 @@ public class ExcelUtilTest {
 
     /**
      * 读取大数据量的Excel
+     *
      * @return
      */
-    private static RowHandler createRowHandler () {
+    private static RowHandler createRowHandler() {
         return new RowHandler() {
             @Override
             public void handle(int sheetIndex, int rowIndex, List<Object> rowlist) {
--- seal-util/src/main/java/com/seal/util/vo/QrPictureVO.java
+++ seal-util/src/main/java/com/seal/util/vo/QrPictureVO.java
@@ -1,7 +1,16 @@
 package com.seal.util.vo;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.seal.util.enums.QrTypeEnum;
 import lombok.Data;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -22,6 +31,69 @@ public class QrPictureVO {
      * 二维规格类型
      */
     private QrTypeEnum qrTypeEnum;
+
+
+    public static void main(String[] args) {
+//        List list = new ArrayList();
+//        int num = 0;
+//        for (int i = 0; i < 10; i++) {
+//            QrPictureVO qrPictureVO = new QrPictureVO();
+//            qrPictureVO.setChannelName("1");
+//            qrPictureVO.setQrTypeEnum(QrTypeEnum.DIMENSION_EIGHT);
+//            list.add(qrPictureVO);
+//            if (list.size() == 2) {
+//                System.out.println("第" + ++num);
+//                list.clear();
+//            }
+//        }
+//        System.out.println("第" + list.size());
+
+        List<BasePostParam> list = new ArrayList<>();
+        BasePostParam qrPictureVO = new BasePostParam();
+        qrPictureVO.setParam("1");
+        qrPictureVO.setSign("1");
+        list.add(qrPictureVO);
+
+        BasePostParam qrPictureVO2 = new BasePostParam();
+        qrPictureVO2.setParam("2");
+        qrPictureVO2.setSign("2");
+        list.add(qrPictureVO2);
+
+
+        List<BasePostParam> list1 = new ArrayList<>();
+        BasePostParam basePostParam = new BasePostParam();
+        basePostParam.setParam("2");
+        basePostParam.setSign("2");
+        list1.add(basePostParam);
+        //list.removeAll(list1);
+        System.out.println(list);
+        try {
+            writeFileContext(list, "G:短信触发的用户.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 将list按行写入到txt文件中
+     *
+     * @param strings
+     * @param path
+     * @throws Exception
+     */
+    public static void writeFileContext(List<BasePostParam> strings, String path) throws Exception {
+        File file = new File(path);
+        //如果没有文件就创建
+        if (!file.isFile()) {
+            file.createNewFile();
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        for (BasePostParam basePostParam : strings) {
+            writer.write(basePostParam.getParam() + "\r\n");
+        }
+        writer.close();
+    }
 
 
 }
