22ece9c5ef6833570d8a7026109400e09fec7f58
youngMen1 <fengzhiqiangzxx@163.com>
2020-05-13 21:08:35 +0800

    desc：seal-test

---
+++ seal-test/src/test/java/com/seal/test/SealTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seal.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
